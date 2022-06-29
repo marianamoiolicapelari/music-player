31ec5a32d3355aa4ba209bf0056cdafa05d744b9
marianamoiolicapelari <marianamoioli@gmail.com>
2022-06-29 14:42:42 -0300

    alt final

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let musicas = [
-  { titulo: 'Guittar Solo', artista: 'Blitz', src: './music/01.mp3', img: './assets/rock.jpg' },
-  { titulo: 'Samba raiz', artista: 'Blitz', src: './music/02.mp3', img: './assets/mpb.jpg' },
-  { titulo: 'Música piano', artista: 'Blitz', src: './music/03.mp3', img: './assets/classic.jpg' },
+  {titulo:'You Know Im No Good', artista:'Amy Winehouse', src:'./music/You Know Im No Good.mp3', img: './assets/Amy Winehouse.jpg'},
+  {titulo:'Loner', artista:'Black Sabbath', src:'./music/Loner.mp3', img: './assets/Black Sabbath.jpg'},
+  {titulo:'Mr Crawley', artista:'Ozzy Osboune', src:'./music/Mr._Crawley.mp3', img: './assets/Ozzy Osboune.jpg'},
+  {titulo:'Territory', artista:'Sepultura', src:'./music/Territory.mp3', img: './assets/Sepultura.jpg'},
+  {titulo:'Angel of Death', artista:'Slayer', src:'./music/Angel of Death.mp3', img: './assets/Slayer.jpg'}
 ]
 
-let indexMusica = 0
 let musica = document.querySelector('audio')
+let indexMusica = 0
 let duracaoMusica = document.querySelector('.fim')
 let imagem = document.querySelector('img')
 let nomeMusica = document.querySelector('.descricao h2')
@@ -13,61 +15,64 @@ let nomeArtista = document.querySelector('.descricao i')
 
 renderizarMusica(indexMusica)
 
-function renderizarMusica(index) {
+
+function renderizarMusica(index){
   musica.setAttribute('src', musicas[index].src)
   musica.addEventListener('loadeddata', () => {
-    nomeMusica.textcontent = musicas[index].titulo
-    nomeArtista.textContent = musicas[index].artista
-    imagem.src = musicas[index].img
-    duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration))
-    pausarMusica()
+      nomeMusica.textContent = musicas[index].titulo
+      nomeArtista.textContent = musicas[index].artista
+      imagem.src = musicas[index].img
+      duracaoMusica.textContent = segundosParaMinutos(Math.floor(musica.duration))
+      pausarMusica()
   })
 }
 
-function tocarMusica() {
+function tocarMusica(){
   musica.play()
   document.querySelector('.botao-pause').style.display = 'block'
   document.querySelector('.botao-play').style.display = 'none'
 }
 
-function pausarMusica() {
+function pausarMusica(){
   musica.pause()
   document.querySelector('.botao-pause').style.display = 'none'
   document.querySelector('.botao-play').style.display = 'block'
 }
 
-function atualizarBarra() {
+function atualizarBarra(){
   let barra = document.querySelector('progress')
   barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + '%'
   let tempoDecorrido = document.querySelector('.inicio')
   tempoDecorrido.textContent = segundosParaMinutos(Math.floor(musica.currentTime))
 }
 
-function segundosParaMinutos(segundos) {
+function segundosParaMinutos(segundos){
   let campoMinutos = Math.floor(segundos / 60)
   let campoSegundos = segundos % 60
-  if (campoSegundos < 10) {
-    campoSegundos = '0' + campoSegundos
+  if (campoSegundos < 10){
+      campoSegundos = '0' + campoSegundos
   }
-  return campoMinutos + ':' + campoSegundos
+
+  return campoMinutos+':'+campoSegundos
 }
-
-
 
 document.querySelector('.botao-play').addEventListener('click', tocarMusica)
 document.querySelector('.botao-pause').addEventListener('click', pausarMusica)
 musica.addEventListener('timeupdate', atualizarBarra)
+
 document.querySelector('.anterior').addEventListener('click', () => {
   indexMusica--
   if (indexMusica < 0) {
-    indexMusica = 2
+      indexMusica = 4
   }
   renderizarMusica(indexMusica)
 })
+
 document.querySelector('.proxima').addEventListener('click', () => {
   indexMusica++
-  if (indexMusica > 2) {
-    indexMusica = 0
+  if (indexMusica > 4){
+      indexMusica = 0
   }
   renderizarMusica(indexMusica)
 })
+
